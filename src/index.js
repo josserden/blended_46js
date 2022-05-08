b1930b96d6588a16ec6563eed0500d3691e4af67
Den <52295780+josserden@users.noreply.github.com>
2022-05-08 10:33:35 +0300

    create template

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import 'material-icons/iconfont/material-icons.css';
+import './sass/main.scss';
+
+import { getRefs } from './js/getRefs';
+const { formSearch, modalForm, contactsList } = getRefs();
---
+++ src/js/getRefs.js
@@ -0,0 +1,7 @@
+export const getRefs = () => {
+  return {
+    formSearch: document.querySelector('.js-search'),
+    modalForm: document.querySelector('.js-modal-form'),
+    contactsList: document.querySelector('.js-contacts-list'),
+  };
+};
