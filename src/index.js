a58cd7beac84363e3ace7e8381a93a5dd4ebdec8
Den <52295780+josserden@users.noreply.github.com>
2022-05-08 13:03:02 +0300

    finish

--- src/index.js
+++ src/index.js
@@ -3,3 +3,28 @@ import './sass/main.scss';
 
 import { getRefs } from './js/getRefs';
 const { formSearch, modalForm, contactsList } = getRefs();
+
+// Event listeners
+modalForm.addEventListener('submit', e => {
+  e.preventDefault();
+  const { name, phone, email } = e.target;
+
+  const userName = name.value.trim();
+  const userPhone = phone.value.trim();
+  const userEmail = email.value.trim();
+
+  if (!userName || !userPhone || !userEmail) {
+    alert('Please fill in all fields');
+    return;
+  }
+
+  const contact = {
+    name: userName,
+    phone: userPhone,
+    email: userEmail,
+  };
+
+  console.log(contact);
+
+  modalForm.reset();
+});
