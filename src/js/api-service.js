7019716a1892479fc21adf11cd340b94aba97455
Den <52295780+josserden@users.noreply.github.com>
2022-06-05 09:57:38 +0300

    add lesson

---
+++ src/js/api-service.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+const API_KEY = '563492ad6f917000010000017177ac82ad294f609aa250f88e62125c';
+axios.defaults.baseURL = 'https://api.pexels.com/v1/';
+axios.defaults.headers.common['Authorization'] = API_KEY;
+axios.defaults.params = {
+  orientation: 'landscape',
+};
